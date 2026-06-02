e2e75f4cfde80aeea17635d694eb71d97ac3b02e
elias <elias.@gmail.com>
2026-06-02 07:43:26 -0700

    update

--- app/index.js
+++ app/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 // Static and routes
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(require('../app/controllers'));
 
 // 404 handler
